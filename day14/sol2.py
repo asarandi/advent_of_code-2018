67071a7b4f017935205f76044e00407abd40f3c0
Alexandr Sarandi <asarandi@student.42.us.org>
2018-12-13 23:07:18 -0800

    day14, part2 w/ deque

--- day14/sol2.py
+++ day14/sol2.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
+from collections import deque
+
 #
 #advent of code 2018
 #day 14
 #part 2
 #
 
-# slow!! re-implement with sliding window
-#e1z4r6p21% time ./sol2.py
-#20261485
-#./sol2.py  34.89s user 0.16s system 99% cpu 35.120 total
-#
-
 data = [3, 7]
 idx_first = 0
 idx_second = 1
 
-puzzle = [6,5,2,6,0,1]
-res = None
+puzzle = deque([6,5,2,6,0,1])
+last = deque([0,0,0,0,0,0])
 while True:
     added = data[idx_first] + data[idx_second]
     if added >= 10:
         data.append(1)
-    if data[len(data) - len(puzzle):] == puzzle:
-        break
+        last.popleft()
+        last.append(1)
+        if last == puzzle:
+            break
     data.append(int(added % 10))
-    if data[len(data) - len(puzzle):] == puzzle:
+    last.popleft()
+    last.append(int(added % 10))
+    if last == puzzle:
         break
     idx_first = int((data[idx_first] + 1 + idx_first) % len(data))
     idx_second = int((data[idx_second] + 1 + idx_second) % len(data))
